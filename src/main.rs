ad88416aef1c30b10ed8e3c5d2e12362647541f8
Derek Chai <chderek9@gmail.com>
2023-09-27 14:35:51 +1300

    Piece/Colour enums, board_setup() function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[derive(Copy, Clone)]
+enum Piece {
+    Pawn(Colour), 
+    Knight(Colour),
+    Bishop(Colour),
+    Rook(Colour),
+    Queen(Colour),
+    King(Colour),
+    Empty,
+}
+
+#[derive(Copy, Clone)]
+enum Colour {
+    Black, 
+    White,
+}
+
 fn main() {
+    board_setup();
     println!("Hello, world!");
+}
+
+fn board_setup() -> [[Piece; 8]; 8] {
+    let mut board = [[Piece::Empty; 8]; 8]; 
+    board
 }
